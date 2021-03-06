14fa3b7ee6c2258d56ec12283949b51f1be3e482
Philipp Strobl <ph.strobl@gmx.de>
2021-03-06 11:13:41 +0100

    #2 created module structure, separated login and edit-questionnaire modules

--- src/app/edit/components/questionnaire-edit/questionnaire-edit.component.ts
+++ src/app/edit/components/questionnaire-edit/questionnaire-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Questionnaire } from '../interfaces/questionnaire';
-import { Question } from '../interfaces/question';
+import { Questionnaire } from '../../../interfaces/questionnaire';
+import { Question } from '../../../interfaces/question';
 import {ActivatedRoute, Router} from '@angular/router';
-import { QuestionnaireService } from '../services/questionnaire.service';
+import { QuestionnaireService } from '../../../services/questionnaire.service';
 
 @Component({
   selector: 'app-questionaire-edit',
